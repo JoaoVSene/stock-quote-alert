0aa6f9cfb511347fecfd4195df44e8364d839a56
JoaoVSene <JoaoVSene@users.noreply.github.com>
2023-12-17 15:21:53 -0300

    feat: add Api Settings reader

--- StockQuoteAlert/Program.cs
+++ StockQuoteAlert/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using StockQuoteAlert;
+using System.Text.Json;
+using static QuoteAPI;
+
+internal static class Program
+{
+    public static async Task Main(string[] args)
+    {
+        string assetName;
+        decimal sellPrice, buyPrice, value;
+        Settings settings;
+
+        string path = Path.Combine(Directory.GetParent(System.IO.Directory.GetCurrentDirectory()).Parent.Parent.Parent.FullName, "settings.json");
+
+        settings = await JsonFileReader.ReadAsync<Settings>(path);
+
+        try
+        {
+            assetName = args[0];
+            sellPrice = decimal.Parse(args[1]);
+            buyPrice = decimal.Parse(args[2]);
+        }
+        catch (Exception e){
+            Console.WriteLine("Error parsing args values:\n"+e.Message);
+            return;
+        }
+
+        Console.WriteLine(settings.Api);
+
+        var api = new QuoteAPI(settings.Api.Token);
+
+        value = await api.GetPrice(assetName);
+        
+        Console.WriteLine("Value: " + value);
+    }
+}
---
+++ StockQuoteAlert/Settings.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
+
+namespace StockQuoteAlert
+{
+    public class API
+    {
+        public string Token { get; set; }
+        public int Delay { get; set; }
+    }
+    public class Settings
+    {
+        public API Api { get; set; }
+    }
+    public static class JsonFileReader
+    {
+        public static async Task<T> ReadAsync<T>(string filePath)
+        {
+            using FileStream stream = File.OpenRead(filePath);
+            return await JsonSerializer.DeserializeAsync<T>(stream);
+        }
+    }
+}
