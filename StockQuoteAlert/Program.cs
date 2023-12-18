810af9039a597a6699052f059da8a459f5d03d40
JoaoVSene <JoaoVSene@users.noreply.github.com>
2023-12-17 22:40:37 -0300

    feat: add class to track the stock quote continuously

--- StockQuoteAlert/Program.cs
+++ StockQuoteAlert/Program.cs
@@ -34,28 +34,8 @@ namespace StockQuoteAlert
             QuoteAPI api = new (settings.Api!.Token!);
             MailManager email = new (settings.Smtp!.Server!, settings.Smtp!.Port!, settings.Smtp!.Username!, settings.Smtp.Password!);
 
-            Stock actualPrice;
-            string action;
-
-            while (true)
-            {
-                actualPrice = await api.GetPrice(assetName);
-                Console.WriteLine("Regular Market Price: " + actualPrice.price + " " + actualPrice.currency + ".");
-                Console.WriteLine(sellPrice + " " + buyPrice);
-
-                if (actualPrice.price > sellPrice)
-                {
-                    Console.WriteLine("Sending emails to sell shares.");
-                    email.SendEmailMessage(settings.Sender!, settings.Recipients!, actualPrice, assetName, "sell");
-                }
-                else if (actualPrice.price < buyPrice)
-                {
-                    Console.WriteLine("Sending emails to buy shares.");
-                    email.SendEmailMessage(settings.Sender!, settings.Recipients!, actualPrice, assetName, "buy");
-                }
-
-                await Task.Delay(settings.Api!.Delay!);
-            }
+            QuoteTracker tracker = new(api, email, settings, assetName, sellPrice, buyPrice);
+            await tracker.TrackerAsync();
         }
     }
 }
---
+++ StockQuoteAlert/QuoteTracker.cs
@@ -0,0 +1,58 @@
+﻿using StockQuoteAlert;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockQuoteAlert
+{
+    internal class QuoteTracker
+    {
+        private readonly QuoteAPI api;
+        private readonly MailManager email;
+        private readonly Settings settings;
+        private readonly string assetName;
+        private readonly decimal sellPrice;
+        private readonly decimal buyPrice;
+
+        public QuoteTracker(QuoteAPI api, MailManager email, Settings settings, string assetName, decimal sellPrice, decimal buyPrice)
+        {
+            this.api = api;
+            this.email = email;
+            this.settings = settings;
+            this.assetName = assetName;
+            this.sellPrice = sellPrice;
+            this.buyPrice = buyPrice;
+        }
+
+        public async Task TrackerAsync()
+        {
+            Stock actualPrice;
+
+            while (true)
+            {
+                actualPrice = await api.GetPrice(this.assetName!);
+                Console.WriteLine("Regular Market Price: " + actualPrice.price + " " + actualPrice.currency +
+                    " at " + DateTime.Now.ToString() + ".");
+
+                if (actualPrice.price > sellPrice)
+                {
+                    Console.WriteLine("Sending emails to sell shares.");
+                    email.SendEmailMessage(settings.Sender!, settings.Recipients!, actualPrice, assetName, "sell");
+                }
+                else if (actualPrice.price<buyPrice)
+                {
+                    Console.WriteLine("Sending emails to buy shares.");
+                    email.SendEmailMessage(settings.Sender!, settings.Recipients!, actualPrice, assetName, "buy");
+                }
+                else
+                {
+                    Console.WriteLine("Sending emails to buy shares.");
+                }
+
+                await Task.Delay(settings.Api!.Delay!);
+            }
+        }
+    }
+}
