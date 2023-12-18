659646e44f3f5ccde1d1a397f5ebd2cb1b4d8b63
JoaoVSene <JoaoVSene@users.noreply.github.com>
2023-12-17 22:07:34 -0300

    refactor: add SMTP and email parameters

--- StockQuoteAlert/Settings.cs
+++ StockQuoteAlert/Settings.cs
@@ -6,24 +6,40 @@ using System.Text;
 using System.Text.Json;
 using System.Text.Json.Nodes;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace StockQuoteAlert
 {
-    public class API
+    public class APISettings
     {
-        public string Token { get; set; }
+        public string? Token { get; set; }
         public int Delay { get; set; }
+    }
+    public class SmtpSettings
+    {
+        public string? Server { get; set; }
+        public int Port { get; set; }
+        public string? Username { get; set; }
+        public string? Password { get; set; }
     }
     public class Settings
     {
-        public API Api { get; set; }
+        public APISettings? Api { get; set; }
+        public SmtpSettings? Smtp { get; set; }
+        public string? Sender { get; set; }
+        public List<string>? Recipients { get; set; }
+
+        public static string createPath(string localPath)
+        {
+            return Path.Combine(Directory.GetParent(System.IO.Directory.GetCurrentDirectory())!.Parent!.Parent!.Parent!.FullName, localPath);
+        }
     }
     public static class JsonFileReader
     {
         public static async Task<T> ReadAsync<T>(string filePath)
         {
-            using FileStream stream = File.OpenRead(filePath);
-            return await JsonSerializer.DeserializeAsync<T>(stream);
+            var stream = File.OpenRead(filePath);
+            return (await JsonSerializer.DeserializeAsync<T>(stream))!;
         }
     }
 }
